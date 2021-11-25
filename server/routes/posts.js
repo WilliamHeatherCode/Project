0679b42909627bfe409c695bf67abb683a097e6b
William Heather (UG) <b9024555@newcastle.ac.uk>
2021-11-25 20:32:10 +0000

    maincommit

---
+++ server/routes/posts.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import { getPosts, createPosts } from '../controllers/posts.js';
+
+
+const router = express.Router();
+
+router.get('/', getPosts)
+router.post('/', createPosts)
+export default router;
+
+// http:/localhost:5000/posts
