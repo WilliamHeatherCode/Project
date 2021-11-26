bf7325694219b1231595913748a758e6ca38d199
William Heather (UG) <b9024555@newcastle.ac.uk>
2021-11-26 15:48:59 +0000

    Adding Images

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import recipes from './images/recipes.png';
 const App = () => {
 
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" alighn="center">Recipes</Typography>
+                <img src={ recipes } alt="recipes" height="60" />
+            </AppBar>
+        </Container>
     )
 }
 
